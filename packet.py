233829a4255c563afe06edea253a53b15f04577c
xuboqing <11812910@mail.sustech.edu.cn>
2020-12-13 20:53:41 +0800

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,29 +1,31 @@
 class Packet:
-    def __init__(self):
-        self.SYN = False
-        self.ACK = False
-        self.FIN = False
-        self.seq = 0
-        self.ack = 0
-        self.LEN = 0
-        self.CHECKSUM = 0
-        self.payload = b''
+    def __init__(self,SEQ=0, SEQ_ACK=0, data=b'', SYN=False, ACK=False, FIN=False):
+        self.SYN = SYN
+        self.ACK = ACK
+        self.FIN = FIN
+        self.SEQ = SEQ
+        self.SEQ_ACK = SEQ_ACK
+        self.LEN = len(data)
+        self.PAYLOAD = data
+        self.CHECKSUM=0
+        self.CHECKSUM = self.checksum(self.to_bytes())
+
 
     def to_bytes(self):
         data = b''
         flag = 0
         if self.SYN:
-            flag += 0x8000
-        if self.ACK:
-            flag += 0x4000
+            flag += 0x0004
         if self.FIN:
-            flag += 0x2000
+            flag += 0x0002
+        if self.ACK:
+            flag += 0x0001
         data += int.to_bytes(flag, 2, byteorder='big')
         data += int.to_bytes(self.CHECKSUM, 2, byteorder='big')
-        data += int.to_bytes(self.seq, 4, byteorder='big')
-        data += int.to_bytes(self.ack, 4, byteorder='big')
+        data += int.to_bytes(self.SEQ, 4, byteorder='big')
+        data += int.to_bytes(self.SEQ_ACK, 4, byteorder='big')
         data += int.to_bytes(self.LEN, 4, byteorder='big')
-        data += self.payload
+        data += self.PAYLOAD
 
         completed = self.LEN % 4
         if completed == 0:
@@ -41,48 +43,31 @@ class Packet:
     def from_bytes(byte: bytes):
         packet = Packet()
         flag = int.from_bytes(byte[0:2], byteorder='big')
-        if flag & 0x8000 != 0:
+        if flag & 0x8004 != 0:
             packet.SYN = True
-        if flag & 0x4000 != 0:
-            packet.ACK = True
-        if flag & 0x2000 != 0:
+        if flag & 0x0002 != 0:
             packet.FIN = True
+        if flag & 0x0001 != 0:
+            packet.ACK = True
         packet.CHECKSUM = int.from_bytes(byte[2:4], byteorder='big')
-        packet.seq = int.from_bytes(byte[4:8], byteorder='big')
-        packet.ack = int.from_bytes(byte[8:12], byteorder='big')
+        packet.SEQ = int.from_bytes(byte[4:8], byteorder='big')
+        packet.SEQ_ACK = int.from_bytes(byte[8:12], byteorder='big')
         packet.LEN = int.from_bytes(byte[12:16], byteorder='big')
-        packet.payload = byte[16:]
+        packet.PAYLOAD = byte[16:]
 
         completed = packet.LEN % 4
         if completed == 0:
             pass
         elif completed == 3:
-            packet.payload = packet.payload[:-1]
+            packet.PAYLOAD = packet.PAYLOAD[:-1]
         elif completed == 2:
-            packet.payload = packet.payload[:-2]
+            packet.PAYLOAD = packet.PAYLOAD[:-2]
         elif completed == 1:
-            packet.payload = packet.payload[:-3]
+            packet.PAYLOAD = packet.PAYLOAD[:-3]
 
-        assert packet.LEN == len(packet.payload)
+###########################################################################3需要修改
+        assert packet.LEN == len(packet.PAYLOAD)
         assert Packet.checksum(packet.to_bytes()) == 0
-
-        return packet
-
-    @staticmethod
-    def create(seq=0, ack=0, data=b'', SYN=False, ACK=False, FIN=False):
-        packet = Packet()
-        packet.ACK = ACK
-        packet.FIN = FIN
-        packet.SYN = SYN
-
-        packet.seq = seq
-        packet.ack = ack
-        packet.LEN = len(data)
-
-        packet.payload = data
-        packet.CHECKSUM = 0
-        checksum = Packet.checksum(packet.to_bytes())
-        packet.CHECKSUM = checksum
 
         return packet
 
@@ -108,13 +93,13 @@ class Packet:
             res += "\033[91mFIN\033[0m "
 
         res += "["
-        res += "seq={}, ".format(self.seq)
-        res += "ack={}, ".format(self.ack)
+        res += "seq={}, ".format(self.SEQ)
+        res += "ack={}, ".format(self.SEQ_ACK)
 
         if self.LEN != 0:
             res += "Len={}, ".format(self.LEN)
             res += "] "
-            res += str(self.payload)
+            res += str(self.PAYLOAD)
         else:
             res += "] "
 
